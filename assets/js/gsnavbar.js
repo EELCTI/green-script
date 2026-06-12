69c32babcce0879e7ec6bd8178deb7aad9e8720a
Mary <maryannequintao3210@gmail.com>
2026-06-12 19:45:36 -0300

    12/06

--- assets/js/gsnavbar.js
+++ assets/js/gsnavbar.js
@@ -7,7 +7,7 @@ export class GreenScriptNavBar extends HTMLElement{
         this.innerHTML = `
         <nav class="navbar navbar-expand-md bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="index.html">Green Script</a>
+            <a class="navbar-brand" href="GreenScript.html">Green Script</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
@@ -16,10 +16,10 @@ export class GreenScriptNavBar extends HTMLElement{
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link" href="">Fale Conosco</a>
+                        <a class="nav-link" href="Faleconosco.html">Fale Conosco</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="sobreNos.html">Quem Somos?</a>
+                        <a class="nav-link" href="QuemSomos.html">Quem Somos?</a>
                     </li>
                 </ul>
                 <form class="d-flex" role="search">
@@ -36,27 +36,4 @@ export class GreenScriptNavBar extends HTMLElement{
 
 }
 
-   <nav class="navbar navbar-expand-md bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="GreenS">Green Script</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link" href="Faleconosco.html">Fale Conosco</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="sobreNos.html">Quem Somos?</a>
-                    </li>
-                </ul>
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
-            </div>
-        </div>
-    </nav>
+   
